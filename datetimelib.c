9bce1e9d0e700439f9eac30960cb6e33a81cee5d
Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>
2019-10-08 13:44:34 -0300

    Add initial version of datetime source
    Signed-off-by: Matheus Tenorio dos Santos <tenoriomatheus0@gmail.com>

---
+++ datetimelib.c
@@ -0,0 +1,36 @@
+#include "datetimelib.h"
+
+bool datetime_is_lerp(u32_t year)
+{
+
+}
+
+bool datetime_is_valid(datetime_t *datetime)
+{
+
+}
+
+int datetime_add_datetime(datetime_t *start, datetime_t *period, datetime_t *result)
+{
+
+}
+
+int datetime_days_between_dates(datetime_t *start, datetime_t *end, u32_t *result)
+{
+
+}
+
+int datetime2timestamp(datetime_t *datetime, timestamp_t *result)
+{
+
+}
+
+int timestamp2datetime(timestamp_t *timestamp, datetime_t *result)
+{
+
+}
+
+int datetime_get_weekday(datetime_t *datetime, weekday_t *result)
+{
+
+}
